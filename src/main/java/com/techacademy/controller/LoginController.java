d5a17edf9fc63243dcc7af3ec9b624bd795b3ca9
smoto <smoto@Ryo-Sakamoto-PC>
2023-05-13 23:21:40 +0900

    Lesson 35Chapter 7.7　認可機能の追加　課題再提出#１

--- src/main/java/com/techacademy/controller/LoginController.java
+++ src/main/java/com/techacademy/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     /** ログイン画面を表示 */
-    @GetMapping({"/", "/login"})
+    @GetMapping({"/","/login"})
     public String getLogin() {
         // login.htmlに画面遷移
         return "login";
---
+++ src/main/java/com/techacademy/controller/TopPageController.java
@@ -0,0 +1,15 @@
+package com.techacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TopPageController {
+    /** トップページ画面を表示 */
+    @GetMapping("/toppage")
+    public String getTopPage() {
+        // toppage.htmlに画面遷移
+        return "toppage";
+    }
+
+}
